8f6f0d521a0902ae8d7f3a5dfd19ca79277f232e
timmeyer1 <timvameyer@gmail.com>
2025-04-18 09:02:55 +0200

    Initial Commit

---
+++ Wine/WineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WineApp.swift
+//  Wine
+//
+//  Created by Stagiaire on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
